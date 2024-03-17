9b30706382239ef0650d545732f98791f9336632
alexsajigeorge <alexsajigeorge@gmail.com>
2024-03-18 02:47:04 +0400

    web route update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/getSkills', [App\Http\Controllers\Admin\SkillsController::class, 'getSkills']);
+Route::get('/getProjects', [App\Http\Controllers\Admin\ProjectController::class, 'getProjects']);
+Route::get('/getProfile', [App\Http\Controllers\Admin\ProfileController::class, 'getProfile']);
+
+Route::get('/getContacts', [App\Http\Controllers\Frontend\ContactController::class, 'getContacts']);
+Route::post('/contactMe', [App\Http\Controllers\Frontend\ContactController::class, 'contactMe']);
+Route::delete('/deleteContact/{id}', [App\Http\Controllers\Frontend\ContactController::class, 'deleteContact']);
+
+Route::get('/getSocialLinks', [App\Http\Controllers\Admin\SocialLinkController::class, 'getSocialLinks']);
